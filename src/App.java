1d2a82fa13e5141ab5b41039ee4919cf5e112fe8
ps29653 <SharmaPrashant@JohnDeere.com>
2020-11-27 21:11:58 +0530

    Implemented own Linked list

--- src/App.java
+++ src/App.java
@@ -2,7 +2,11 @@ class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello");
+        MyLinkedList myLinkedList = new MyLinkedList(10);
+        myLinkedList.append(12);
+        myLinkedList.append(14);
+        myLinkedList.append(16);
+        myLinkedList.append(18);
 
     }
 }
---
+++ src/MyLinkedList.java
@@ -0,0 +1,38 @@
+public class MyLinkedList {
+
+    Item head;
+
+    Item tail;
+
+    int size = 0;
+
+    public MyLinkedList(int value) {
+
+        this.head = new Item(value, null);
+        this.tail = this.head;
+        size++;
+    }
+
+    public void append(int newValue) {
+
+        Item newItem = new Item(newValue, null);
+        this.tail.next = newItem;
+        this.tail = newItem;
+        size++;
+
+    }
+
+    class Item {
+
+        int value;
+
+        Item next;
+
+        Item(int v, Item next) {
+            this.value = v;
+            this.next = next;
+        }
+
+    }
+
+}
