4db289047913e7918958cf9d3950f573357b5783
Prashant Sharma <prashant.sharma1thoughtworks.com>
2021-10-31 18:51:17 +0530

    intersection-of-two-linked-lists | O(n)

---
+++ src/IntersectionNode.java
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode.com/problems/intersection-of-two-linked-lists/
+ */
+public class IntersectionNode {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        int lenA = length(headA);
+        int lenB = length(headB);
+
+        while (lenA > lenB) {
+            headA = headA.next;
+            lenA--;
+        }
+        while (lenA < lenB) {
+            headB = headB.next;
+            lenB--;
+        }
+
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+
+        return headA;
+    }
+
+    private int length(ListNode node) {
+        int length = 0;
+        while (node != null) {
+            node = node.next;
+            length++;
+        }
+
+        return length;
+    }
+}
