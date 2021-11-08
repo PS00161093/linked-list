b7a1d51365ac6287c3e3df350e3bcb8ab70853b4
Prashant Sharma <prashant.sharma1thoughtworks.com>
2021-11-08 22:31:10 +0530

    reverse-linked-list-ii

---
+++ src/medium/ReverseBetween.java
@@ -0,0 +1,32 @@
+package medium;
+
+import hard.ListNode;
+
+/**
+ * https://leetcode.com/problems/reverse-linked-list-ii/
+ */
+public class ReverseBetween {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        while (left < right) {
+            ListNode lef = getNodeAt(left, head);
+            ListNode rig = getNodeAt(right, head);
+            int temp = lef.val;
+            lef.val = rig.val;
+            rig.val = temp;
+
+            left++;
+            right--;
+        }
+        return head;
+    }
+
+    public ListNode getNodeAt(int idx, ListNode head) {
+        ListNode c = head;
+        for (int i = 1; i < idx; i++) {
+            c = c.next;
+        }
+
+        return c;
+    }
+}
